1f2ffaa9aab3b7b931ba6234729b5d82d368ab25
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-09-30 17:35:14 -0600

    useOrder custom hook implemented

---
+++ src/hooks/useOrder.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import type { MenuItem, OrderItem } from "../types"
+
+function useOrder() {
+  const [order, setOrder] = useState<OrderItem[]>([])
+
+  const addItem = (item: MenuItem) => {
+    const itemExists = order.find(orderItem => orderItem.id === item.id)
+    if( !itemExists ) {
+      setOrder([...order, {...item, quantity: 1}])
+      return
+    }
+
+    const updatedOrder = order.map( orderItem => {
+      if(orderItem.id === item.id){
+        return{
+          ...orderItem,
+          quantity: orderItem.quantity + 1
+        }
+      }
+
+      return orderItem
+    })
+    setOrder(updatedOrder)
+  }
+
+  const removeItem = (id: number) => {
+    const updatedOrder = order.filter((orderItem) => orderItem.id !== id )
+    setOrder(updatedOrder)
+  }
+
+  return {
+    order,
+    addItem,
+    removeItem
+  }
+}
+
+export default useOrder
